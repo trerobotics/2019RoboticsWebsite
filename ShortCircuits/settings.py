eb15ea24753f4848b6e697a687daf6848efa3201
Ryan gilmartin <ryan22gil@gmail.com>
2018-12-08 23:11:33 -0500

    fixed spelling mistake

--- ShortCircuits/settings.py
+++ ShortCircuits/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0=juii%w@tey&aw_&%10yn)xi(a-iz_b8w)(=7un36jzn9n=$)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = false
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
